229f20234065daeb105be52060b6fe03d3ec52f7
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-09 16:19:36 -0600

    added item to list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import List from './List';
+import TodoForm from './TodoForm'
 
 class App extends Component {
   state = {
@@ -10,13 +11,30 @@ class App extends Component {
     ]
   }
 
+  getUniqId = () => {
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+   }
+
+  addItem = (name) => {
+    const {togets} = this.state
+    const toget = {name, id: this.getUniqId(), complete: false}
+    this.setState({togets: [toget,...togets]})
+  }
+
   
 
 
   render() {
     const {togets} = this.state
     return(
-      <List name="Grocery List" items={togets}/>
+      
+      <div>
+        <TodoForm addItem={this.addItem} />
+        <List name="Grocery List" items={togets}/>
+      </div>
     )
   }
 }
