cbb531f347680c5a5489843a29a72f92b590e054
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-09 15:51:08 -0600

    added list.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    togets: [
+      {id: 1, name: 'veggies', complete: true, },
+      {id: 2, name: 'meats', complete: false, },
+      {id: 3, name: 'breads', complete: false, },
+    ]
+  }
+
+  
+
+
+  render() {
+    const {togets} = this.state
+    return(
+      <List name="Grocery List" items={togets}/>
+    )
+  }
 }
 
 export default App;
